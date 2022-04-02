616bb8af46e9896c91896564a00c7e5518f3d5fe
andregugelmin <andregugelmin1996@gmail.com>
2022-04-02 18:36:11 -0300

    Fixed body background css

--- src/components/layouts/Container.jsx
+++ src/components/layouts/Container.jsx
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    font-family: 'Lexend Deca';
-    background: #F2F2F2;
-    padding-top: 100px;
-    padding-left: 18px;
-    padding-right: 18px;
-    h1{
-        font-family: 'Lexend Deca';
-        font-size: 23px;
-        line-height: 29px;
-        color: #126BA5;
-    }
+	font-family: "Lexend Deca";
+	background: #f2f2f2;
+	padding-top: 100px;
+	padding-left: 18px;
+	padding-right: 18px;
+	height: 100vh;
+
+	h1 {
+		font-family: "Lexend Deca";
+		font-size: 23px;
+		line-height: 29px;
+		color: #126ba5;
+	}
 `;
 
 export default Container;
